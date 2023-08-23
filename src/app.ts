964f9a7934897a1e32e00a1cc6eec0b05675800d
HoseokYoo <hsyoo@pmirnc.com>
2023-08-23 18:11:35 +0900

    refactor(refactoring): 코드 리팩토링
    코드 리팩토링

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,19 @@
-import 'dotenv/config'
-import OpenAI from 'openai';
+import 'dotenv/config';
 import express from 'express';
 import session from 'express-session';
 import bodyParser from 'body-parser';
 import path from 'path';
 import subscribe from './lib/subscribe';
-import { getSubscriber } from './lib/store';
 import { requestAPI } from './lib/openai.api';
-const openai  = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY
-});
 
 const app = express();
 const maxAge = 1e3 * 60 * 60 * 24;
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET || 'secret',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false, maxAge }
+};
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -20,18 +21,10 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('trust proxy', 1);
-app.use(session({
-  secret: process.env.SESSION_SECRET || 'secret',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false, maxAge }
-}));
+app.use(session(sessionOptions));
 
-// app.get('/subscribe', subscribe);
-app.get('/', (req, res) => (res.render('index')));
+app.route('/',).get((req, res) => (res.render('index')));
 
-app.route('/openai')
-  .get(subscribe)
-  .post(requestAPI);
+app.route('/openai').get(subscribe).post(requestAPI);
 
 app.listen(3000, () => {});
