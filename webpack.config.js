004a19d61eb22a743173253c2c5e25a948818e72
HoseokYoo <hsyoo@pmirnc.com>
2023-08-23 11:40:03 +0900

    init

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const nodeExternals = require('webpack-node-externals');
+const CopyPlugin = require("copy-webpack-plugin");
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+    target: 'node',
+    mode: 'production',
+    externals: [nodeExternals()],
+    entry: './src/api.ts',
+    output: {
+        filename: 'api.js',
+        path: __dirname + '/dist'
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+        modules: ['node_modules'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: 'ts-loader'
+            },
+        ]
+    },
+    plugins: [
+        new Dotenv(), // MARK: 별로 좋아보이진 않는다
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: "src/public",
+                    to: "public"
+                },
+                {
+                    from: "src/views",
+                    to: "views"
+                }
+            ]
+        })
+    ]
+}
